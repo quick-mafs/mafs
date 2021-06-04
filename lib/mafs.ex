f2ffd2fa5b72b5f107c1a1dd16e32f8522e7d70a
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-06-03 17:41:45 -0700

    chore: Init repository

---
+++ lib/mafs.ex
@@ -0,0 +1,18 @@
+defmodule Mafs do
+  @moduledoc """
+  Documentation for `Mafs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mafs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mafs_test.exs
@@ -0,0 +1,8 @@
+defmodule MafsTest do
+  use ExUnit.Case
+  doctest Mafs
+
+  test "greets the world" do
+    assert Mafs.hello() == :world
+  end
+end
